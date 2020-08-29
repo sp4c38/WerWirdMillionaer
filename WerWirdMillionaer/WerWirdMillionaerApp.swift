4ec5a393da4ce7881dbbd7da98fe07e46f764901
sp4c38 <36799235+sp4c38@users.noreply.github.com>
2020-08-29 17:57:57 +0200

    Added initial xcode project

---
+++ WerWirdMillionaer/WerWirdMillionaerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WerWirdMillionaerApp.swift
+//  WerWirdMillionaer
+//
+//  Created by Léon Becker on 29.08.20.
+//
+
+import SwiftUI
+
+@main
+struct WerWirdMillionaerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
